03501a1a5d43f17a7029b09ee0e19848635c2714
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-04-25 16:27:38 +0200

    Update Gun.java

--- shipgame-code/src/main/java/dk/kea/shipgame/miscellaneous/Gun.java
+++ shipgame-code/src/main/java/dk/kea/shipgame/miscellaneous/Gun.java
@@ -5,33 +5,32 @@ package dk.kea.shipgame.miscellaneous;
 public class Gun {
 
 
-
-   public static int CritHit(int dice, int hull_health, int currentAmmunitionType){
+   public void CritHit(int dice, int hull_health, int currentAmmunitionType, int sailors, int speed){
         if (dice == 1 && currentAmmunitionType == 0){
             hull_health = hull_health/0;
-            return hull_health;
-            System.out.println("Your ship has exploded");
-
+            System.out.println("Your ship has exploded!");
 
         }
         else if (dice == 2 && currentAmmunitionType == 0) {
-            hull_health = hull_health / 2;
-            return hull_health;
+            hull_health = hull_health/2;
             System.out.println("It is not possible to turn anymore");
 
         }
 
         else if (dice == 3 && currentAmmunitionType == 0){
             hull_health = hull_health/3;
-            return hull_health;
         }
 
+        if (dice == 1 && currentAmmunitionType == 1){
+            speed = 0;
 
 
+       }
 
 
+       }
+
+   }
 
 
-        }
-}
 
