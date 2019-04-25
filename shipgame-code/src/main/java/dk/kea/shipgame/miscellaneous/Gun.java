0b07e5c9b5084a240ece9cd93c4eae69340b1864
leop0006 <43268762+leop0006@users.noreply.github.com>
2019-04-25 17:23:13 +0200

    Update Gun.java

--- shipgame-code/src/main/java/dk/kea/shipgame/miscellaneous/Gun.java
+++ shipgame-code/src/main/java/dk/kea/shipgame/miscellaneous/Gun.java
@@ -1,59 +1,51 @@
-package dk.kea.shipgame.miscellaneous;
-
-
-
-public class Gun {
-
-
-<<<<<<< HEAD
-   public void CritHit(int dice, int hull_health, int currentAmmunitionType, int sailors, int speed){
-        if (dice == 1 && currentAmmunitionType == 0){
-            hull_health = hull_health/0;
-            System.out.println("Your ship has exploded!");
-
-        }
-        else if (dice == 2 && currentAmmunitionType == 0) {
-            hull_health = hull_health/2;
-=======
-    public static int CritHit(int dice, int hull_health, int currentAmmunitionType) {
-        if (dice == 1 && currentAmmunitionType == 0) {
-            hull_health = hull_health / 0;
-            System.out.println("Your ship has exploded");
-            return hull_health;
-
-
-        } else if (dice == 2 && currentAmmunitionType == 0) {
-            hull_health = hull_health / 2;
->>>>>>> 6d404d933a0d1c0b760cf4eca69dcba6db03f64a
-            System.out.println("It is not possible to turn anymore");
-            return hull_health;
-
-<<<<<<< HEAD
-        }
-
-        else if (dice == 3 && currentAmmunitionType == 0){
-            hull_health = hull_health/3;
-        }
-
-        if (dice == 1 && currentAmmunitionType == 1){
-            speed = 0;
-
-
-       }
-
-
-       }
-
-   }
-
-
-=======
-        } else if (dice == 3 && currentAmmunitionType == 0) {
-            hull_health = hull_health / 3;
-            return hull_health;
-        }
-
-        return 0;
-    }
-}
->>>>>>> 6d404d933a0d1c0b760cf4eca69dcba6db03f64a
+package dk.kea.shipgame.miscellaneous;                                                                                            
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+public class Gun {                                                                                                                
+                                                                                                                                  
+                                                                                                                                  
+   public void CritHit(int dice, int hull_health, int currentAmmunitionType, int sailors, int speed, int sail_health){            
+        if (dice == 1 && currentAmmunitionType == 0){                                                                             
+            hull_health = hull_health/0;                                                                                          
+            System.out.println("Your ship has exploded!");                                                                        
+                                                                                                                                  
+        }                                                                                                                         
+        else if (dice == 2 && currentAmmunitionType == 0) {                                                                       
+            hull_health = hull_health/2;                                                                                          
+                                                                                                                                  
+                                                                                                                                  
+        } else if (dice == 2 && currentAmmunitionType == 0) {                                                                     
+            hull_health = hull_health/2;                                                                                          
+            System.out.println("It is not possible to turn anymore");                                                             
+                                                                                                                                  
+        }                                                                                                                         
+                                                                                                                                  
+        else if (dice == 3 && currentAmmunitionType == 0){                                                                        
+            hull_health = hull_health/3;                                                                                          
+        }                                                                                                                         
+                                                                                                                                  
+        if (dice == 1 && currentAmmunitionType == 1){                                                                             
+            speed = 0;                                                                                                            
+                                                                                                                                  
+       }                                                                                                                          
+                                                                                                                                  
+       else if (dice == 2 && currentAmmunitionType == 1){                                                                         
+           sail_health = sail_health/3;                                                                                           
+                                                                                                                                  
+       }                                                                                                                          
+                                                                                                                                  
+        if (dice == 1 && currentAmmunitionType == 2){                                                                             
+                                                                                                                                  
+         sailors = sailors/2;                                                                                                     
+       }                                                                                                                          
+                                                                                                                                  
+       else if(dice == 2 && currentAmmunitionType == 2){                                                                          
+           sailors = sailors /                                                                                                    
+                                                                                                                                  
+                                                                                                                                  
+       }                                                                                                                          
+                                                                                                                                  
+   }                                                                                                                              
+                                                                                                                                  
+}                                                                                                                                 
